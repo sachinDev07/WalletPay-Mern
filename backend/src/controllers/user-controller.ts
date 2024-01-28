6c6c132346cad02c5f8f48b92e7ec15fd386f91d
sachinDev07 <sachinkrj077@gmail.com>
2024-01-29 01:02:57 +0530

    handled user input validation with zod

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,18 +1,36 @@
 import { Request, Response } from "express";
+import zod from "zod";
 
 import User from "../model/user";
 
+const userSchema = zod.object({
+  username: zod
+    .string()
+    .min(3, { message: "Must be 3 or more characters long" }),
+  email: zod.string().email({ message: "Invalid email address" }),
+  password: zod
+    .string()
+    .min(5, { message: "Must be 5 or more characters long" }),
+});
+
 async function signup(req: Request, res: Response) {
   try {
-    const existedUser = await User.findOne({ email: req.body.email });
+    const validatedUserData = userSchema.parse(req.body);
+    const existedUser = await User.findOne({ email: validatedUserData.email });
     if (existedUser) {
-     return res.status(400).json({ message: "User already exists with given email" });
+      return res
+        .status(400)
+        .json({ message: "User already exists with given email" });
     }
 
-    const user = await User.create(req.body);
+    const user = await User.create(validatedUserData);
     await user.save();
     return res.status(200).json({ user, message: "User created successfully" });
   } catch (error) {
+    if (error instanceof zod.ZodError) {
+      return res.status(400).json({ error: error.errors });
+    }
+
     console.error(error);
     res.status(500).json({ error: "Something went wrong!" });
   }
