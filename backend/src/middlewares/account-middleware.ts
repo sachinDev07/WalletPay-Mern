4429d838c25a73d3dba60cae4df99e4b3abd1475
sachinDev07 <sachinkrj077@gmail.com>
2024-02-04 19:03:08 +0530

    Added middleware for handling transfer api

---
+++ backend/src/middlewares/account-middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import zod from "zod";
+
+const transferSchema = zod.object({
+  amount: zod
+    .number()
+    .gt(0, { message: "Amount must be greater than zero" })
+    .nonnegative({ message: "Amount must be a non-negative number" }),
+  to: zod.string(),
+});
+
+async function validateTransferRequest(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    req.body.amount = parseFloat(req.body.amount);
+    transferSchema.parse(req.body);
+    next();
+  } catch (error: any) {
+    if (error instanceof zod.ZodError) {
+      return res.status(400).json({ error: error.errors });
+    }
+
+    console.error(error);
+    res.status(400).json({ error: "Invalid transfer data" });
+  }
+}
+
+export { validateTransferRequest };
--- backend/src/routes/account-routes.ts
+++ backend/src/routes/account-routes.ts
@@ -2,13 +2,14 @@ import express from "express";
 
 import * as AccountController from "../controllers/account-controller";
 import { checkAuth } from "../middlewares/auth-middlewares";
+import { validateTransferRequest } from "../middlewares/account-middleware";
 
 const router = express.Router();
 
 
 router.get("/account/balance", checkAuth, AccountController.getUserBalance);
 
-router.post("/account/transfer", checkAuth, AccountController.handleTransfer);
+router.post("/account/transfer", checkAuth, validateTransferRequest, AccountController.handleTransfer);
 
 
 export default router;
