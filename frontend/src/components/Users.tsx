b55c86c70cbea1b3988aa19394dfa844b2ff8498
sachinDev07 <sachinkrj077@gmail.com>
2024-02-06 20:48:16 +0530

    Added users component ui

---
+++ frontend/src/components/Users.tsx
@@ -0,0 +1,15 @@
+export const Users = () => {
+  return (
+    <>
+      <div className="font-bold mt-6 text-lg">Users</div>
+      <div className="my-2">
+        <input
+          type="text"
+          placeholder="Search users..."
+          className="w-full px-2 py-1 border rounded border-slate-200"
+        ></input>
+      </div>
+    </>
+  );
+};
+export default Users;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import Balance from "../components/Balance";
+import Users from "../components/Users";
 
 const Dashboard = () => {
   return (
     <div className="px-12">
       <div className="m-8">
         <Balance value={10000} />
+        <Users />
       </div>
     </div>
   );
