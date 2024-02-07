500ba529e1a7e827fb52f67ba8f73fad7ba2ea18
sachinDev07 <sachinkrj077@gmail.com>
2024-02-07 19:32:38 +0530

    title name changed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="shadow h-14 px-6 flex justify-between items-center">
-      <div className="text-xl font-bold">Payment App</div>
+      <div className="text-xl font-bold">WalletPay</div>
       <div className="flex justify-between items-center space-x-4">
         <div className="font-bold">Hello</div>
         <div className="rounded-full h-10 w-10 bg-slate-200 text-xl font-bold flex justify-center items-center cursor-pointer">
