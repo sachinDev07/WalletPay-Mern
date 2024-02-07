5a2652546d811ba01120ad9fba05e6256109b0b0
sachinDev07 <sachinkrj077@gmail.com>
2024-02-08 00:08:31 +0530

    fixes header component conditionally

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -6,7 +6,7 @@ import SubHeading from "../components/SubHeading";
 
 const SignIn = () => {
   return (
-    <div className="bg-slate-300 h-[92vh] flex justify-center">
+    <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 p-2 h-max px-4">
           <Heading label={"Sign In"} />
