58ef94ebdd10c1445767e9909a1bf31da91be506
sachinDev07 <sachinkrj077@gmail.com>
2024-02-09 20:57:22 +0530

    Added signup api

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -1,15 +1,18 @@
-const InputBox = ({
-  lable,
-  placeholder,
-}: {
+import { ChangeEvent } from "react";
+
+interface InputBoxProps {
   lable: string;
   placeholder?: string;
-}) => {
+  onchange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputBox = ({ lable, placeholder, onchange }: InputBoxProps) => {
   return (
     <div className="mb-2">
       <label className="font-medium text-left py-2">
         {lable}
         <input
+          onChange={onchange}
           placeholder={placeholder}
           className="block w-full  px-2 py-1 border border-slate-200 rounded"
         />
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,23 +1,83 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 import BottomWarning from "../components/BottomWarning";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import InputBox from "../components/InputBox";
 import SubHeading from "../components/SubHeading";
+import axios from "axios";
+
+interface UserDetails {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
 
 const SignUp = () => {
+  const [firstname, setFirstname] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  async function signupApi() {
+    try {
+      const response = await axios.post<UserDetails>(
+        "https://localhost:7001/api/v1/users/signup",
+        { firstname, lastname, email, password },
+      );
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    signupApi();
+  }
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 p-2 h-max px-4">
           <Heading label={"Sign Up"} />
           <SubHeading label={"Enter your information to create an account"} />
-          <InputBox lable={"first Name"} placeholder={"John"} />
-          <InputBox lable={"last Name"} placeholder={"Doe"} />
-          <InputBox lable={"Email"} placeholder={"example@gmail.com"} />
-          <InputBox lable={"Password"} placeholder={"1x2y3z@password"} />
-          <div className="pt-4 text-center">
-            <Button label={"Sign up"} />
-          </div>
+          <form onSubmit={handleSubmit} action="">
+            <InputBox
+              onchange={(e: ChangeEvent<HTMLInputElement>) =>
+                setFirstname(e.target.value)
+              }
+              lable={"first Name"}
+              placeholder={"John"}
+            />
+            <InputBox
+              onchange={(e: ChangeEvent<HTMLInputElement>) =>
+                setLastname(e.target.value)
+              }
+              lable={"last Name"}
+              placeholder={"Doe"}
+            />
+            <InputBox
+              onchange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              lable={"Email"}
+              placeholder={"example@gmail.com"}
+            />
+            <InputBox
+              onchange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+              lable={"Password"}
+              placeholder={"1x2y3z@password"}
+            />
+            <div className="pt-4 text-center">
+              <Button label={"Sign up"} />
+            </div>
+          </form>
+
           <BottomWarning
             label={"Already have an account?"}
             bottomText={"Sign in"}
