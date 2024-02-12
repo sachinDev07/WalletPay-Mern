d0de6a5a5b8142366bec73f62b7ceaa59ef98c23
sachinDev07 <sachinkrj077@gmail.com>
2024-02-13 01:21:15 +0530

    Added sign up form data using react hook form

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,44 +1,45 @@
-import { ChangeEvent, useState } from "react";
-
-import BottomWarning from "../components/BottomWarning";
-import Button from "../components/Button";
-import Heading from "../components/Heading";
-import InputBox from "../components/InputBox";
-import SubHeading from "../components/SubHeading";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-interface UserDetails {
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+import BottomWarning from "../components/BottomWarning";
+
+type FormData = {
   firstname: string;
   lastname: string;
   email: string;
   password: string;
+};
+
+interface UserDetails {
+  data: FormData;
+  message: string;
 }
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const [firstname, setFirstname] = useState<string>("");
-  const [lastname, setLastname] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
-
-  async function signupApi() {
+  const onSubmit = async (data: FormData) => {
     try {
-      await axios.post<UserDetails>(
+      const response = await axios.post<UserDetails>(
         "http://localhost:7001/api/v1/users/signup",
-        { firstname, lastname, email, password },
+        data,
       );
+      const responseData = response.data;
+      toast.success(responseData.message);
+      navigate("/signin");
     } catch (error) {
-      console.error("err" + error);
+      console.error("Error: ", error);
     }
-  }
-
-  function handleSubmit() {
-    signupApi();
-    navigate("/signin");
-  }
+  };
 
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
@@ -46,37 +47,80 @@ const SignUp = () => {
         <div className="rounded-lg bg-white w-80 p-2 h-max px-4">
           <Heading label={"Sign Up"} />
           <SubHeading label={"Enter your information to create an account"} />
-          <InputBox
-            onchange={(e: ChangeEvent<HTMLInputElement>) =>
-              setFirstname(e.target.value)
-            }
-            lable={"first Name"}
-            placeholder={"John"}
-          />
-          <InputBox
-            onchange={(e: ChangeEvent<HTMLInputElement>) =>
-              setLastname(e.target.value)
-            }
-            lable={"last Name"}
-            placeholder={"Doe"}
-          />
-          <InputBox
-            onchange={(e: ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            lable={"Email"}
-            placeholder={"example@gmail.com"}
-          />
-          <InputBox
-            onchange={(e: ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-            lable={"Password"}
-            placeholder={"1x2y3z@password"}
-          />
-          <div className="pt-4 text-center">
-            <Button onclick={handleSubmit} label={"Sign up"} />
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-2">
+              <label className="font-medium text-left py-2">
+                First Name
+                <input
+                  type="text"
+                  {...register("firstname", {
+                    required: "This field is required",
+                  })}
+                  placeholder="First name"
+                  className="block w-full  px-2 py-1 border border-slate-200 rounded"
+                />
+              </label>
+            </div>
+            {errors.firstname && (
+              <span className="text-red-500">{errors.firstname.message}</span>
+            )}
+            <div className="mb-2">
+              <label className="font-medium text-left py-2">
+                Last Name
+                <input
+                  type="text"
+                  {...register("lastname", {
+                    required: "This field is required",
+                  })}
+                  placeholder="Last name"
+                  className="block w-full  px-2 py-1 border border-slate-200 rounded"
+                />
+              </label>
+            </div>
+            {errors.lastname && (
+              <span className="text-red-500">{errors.lastname.message}</span>
+            )}
+            <div className="mb-2">
+              <label className="font-medium text-left py-2">
+                Email Address
+                <input
+                  type="email"
+                  {...register("email", { required: "This field is required" })}
+                  placeholder="Enter your email address"
+                  className="block w-full  px-2 py-1 border border-slate-200 rounded"
+                />
+              </label>
+            </div>
+            {errors.email && (
+              <span className="text-red-500">{errors.email.message}</span>
+            )}
+            <div className="mb-2">
+              <label className="font-medium text-left py-2">
+                Password
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: "This field is required",
+                  })}
+                  placeholder="Enter your password"
+                  className="block w-full  px-2 py-1 border border-slate-200 rounded"
+                />
+              </label>
+            </div>
+            {errors.password && (
+              <span className="text-red-500">{errors.password.message}</span>
+            )}
+            <div className="pt-4 text-center">
+              <div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </form>
 
           <BottomWarning
             label={"Already have an account?"}
