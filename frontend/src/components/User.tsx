43a20d4b7543c3013a65235786e1d956018b01ab
sachinDev07 <sachinkrj077@gmail.com>
2024-02-14 20:05:17 +0530

    Handled the filter user api

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -13,7 +13,7 @@ const User = ({
     <div className="flex justify-between mt-4">
       <div className="flex">
         <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-          <div className="flex flex-col justify-center h-full text-xl">
+          <div className="flex flex-col justify-center h-full text-xl font-bold">
             {firstChar}
           </div>
         </div>
--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,18 +1,77 @@
+import { useEffect, useState } from "react";
 import User from "./User";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+interface User {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+
+interface ValidationError {
+  message: string;
+  errors: Record<string, string[]>;
+}
 
 export const Users = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [filter, setFilter] = useState<string>("");
+  const getUsers = async (filter: string) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        throw new Error("JWT token is not found");
+      }
+      const response = await axios.get<{ data: User[] }>(
+        "http://localhost:7001/api/v1/users?filter=" + filter,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = response.data;
+      setUsers(data.data);
+    } catch (error) {
+      if (axios.isAxiosError<ValidationError>(error)) {
+        if (error.code === "ERR_BAD_REQUEST") {
+          toast.error("No user found");
+        } else {
+          toast.error("An error occurred");
+        }
+      } else {
+        toast.error("An error occurred");
+      }
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers(filter);
+  }, [filter]);
+
   return (
     <>
       <div className="font-bold mt-6 text-lg">Users</div>
       <div className="my-2">
         <input
+          onChange={(e) => setFilter(e.target.value)}
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded border-slate-400"
         ></input>
       </div>
       <div>
-        <User firstChar={"S"} firstName={"Sachin"} lastName={"Kumar"} />
+        {users?.map((user) => (
+          <User
+            key={user._id}
+            firstChar={user.firstname.charAt(0)}
+            firstName={user.firstname}
+            lastName={user.lastname}
+          />
+        ))}
       </div>
     </>
   );
