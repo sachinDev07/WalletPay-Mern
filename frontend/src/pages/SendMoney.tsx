1e502808c01f95cf4d0012175323ef438b329952
sachinDev07 <sachinkrj077@gmail.com>
2024-02-15 19:58:44 +0530

    Added name using useSearchParam hook

--- frontend/src/pages/SendMoney.tsx
+++ frontend/src/pages/SendMoney.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 export const SendMoney = () => {
+  const [searchParams] = useSearchParams();
+  const name = searchParams.get("name");
+
+  const [amount, setAmount] = useState<number>(0);
+
   return (
-    <div className="flex justify-center h-screen bg-gray-100">
+    <div className="flex justify-center h-[calc(100vh-3.5rem)] bg-gray-100">
       <div className="h-full flex flex-col justify-center">
         <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
           <div className="flex flex-col space-y-1.5 px-6 pt-6">
@@ -9,11 +17,13 @@ export const SendMoney = () => {
           <div className="px-6 pb-6">
             <div className="flex items-center space-x-4">
               <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span className="text-2xl text-white">A</span>
+                <span className="text-2xl font-bold text-white">
+                  {name?.charAt(0)}
+                </span>
               </div>
-              <h3 className="text-2xl font-semibold">Friend's Name</h3>
+              <h3 className="text-3xl font-semibold">{name}</h3>
             </div>
-            <div className="space-y-4">
+            <div className="space-y-6 mt-4">
               <div className="space-y-2">
                 <label
                   className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -26,6 +36,7 @@ export const SendMoney = () => {
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
+                  onChange={(e) => setAmount(+e.target.value)}
                 />
               </div>
               <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white hover:bg-green-700 duration-150 ease-linear">
