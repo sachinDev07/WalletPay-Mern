44c7d96f15e94c1cc5b1aed1fee34152d02e935a
sachinDev07 <sachinkrj077@gmail.com>
2024-02-16 17:27:07 +0530

    Implemented balance api for balance

--- frontend/src/components/Balance.tsx
+++ frontend/src/components/Balance.tsx
@@ -1,9 +1,9 @@
-const Balance = ({ value }: { value: number }) => {
+const Balance = ({ value }: { value: number | null }) => {
   return (
     <div className="flex">
       <div className="font-bold text-lg">Your balance:</div>
       <div className="font-medium ml-4 text-lg">
-        Rs {value.toLocaleString()}
+        Rs {value?.toLocaleString()}
       </div>
     </div>
   );
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
 import Balance from "../components/Balance";
 import Users from "../components/Users";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
+  const [balance, setBalance] = useState<number | null>();
+
+  useEffect(() => {
+    getUserBalanceApi();
+  }, []);
+
+  async function getUserBalanceApi() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      toast.info("User is not authorized!");
+      return;
+    }
+    try {
+      const response = await axios.get<{ balance: number }>(
+        "http://localhost:7001/api/v1/account/balance",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = response.data;
+      setBalance(data.balance);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="px-12">
       <div className="m-8">
-        <Balance value={10000} />
+        <Balance value={balance} />
         <Users />
       </div>
     </div>
