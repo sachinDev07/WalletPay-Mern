f9ead3fd750d2985569e548f870d116e93ea2e8d
sachinDev07 <sachinkrj077@gmail.com>
2024-03-08 16:46:02 +0530

    Created character logo component

---
+++ frontend/src/components/CharacterLogo.tsx
@@ -0,0 +1,37 @@
+import classNames from "classnames";
+
+type CharacterLogoProps = {
+  character: string;
+  width: string;
+  height: string;
+  bgColor: string;
+  textColor: string;
+  textSize: string;
+};
+
+const CharacterLogo = ({
+  character,
+  width,
+  height,
+  bgColor,
+  textColor,
+  textSize,
+}: CharacterLogoProps) => {
+  const classStr = classNames(
+    "rounded-full flex justify-center",
+    width,
+    height,
+    bgColor,
+    textColor,
+    textSize,
+  );
+  return (
+    <div className={classStr}>
+      <div className={`flex flex-col justify-center h-full font-bold`}>
+        {character.toUpperCase()}
+      </div>
+    </div>
+  );
+};
+
+export default CharacterLogo;
