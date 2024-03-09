3f6f75220869864e7461e91022410be55a9e86f8
sachinDev07 <sachinkrj077@gmail.com>
2024-03-09 17:18:22 +0530

    Implemented get notifications api

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -2,12 +2,23 @@ import { MdOutlineDeleteOutline } from "react-icons/md";
 import CharacterLogo from "./CharacterLogo";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
 
-const Notification = () => {
+type NotificationType = {
+  amount: number;
+  name: string;
+  read: boolean;
+  date: string;
+};
+
+const Notification = ({ amount, name, read, date }: NotificationType) => {
   return (
-    <div className="mt-2 p-1 flex justify-between items-center bg-gray-200 rounded-md">
+    <div
+      className={`${
+        read ? "bg-gray-200" : "bg-gray-400"
+      } mt-2 p-1 flex justify-between items-center rounded-md`}
+    >
       <div className="flex items-center justify-between">
         <CharacterLogo
-          character="S"
+          character={name.charAt(0).toUpperCase()}
           width="w-6"
           height="h-6"
           bgColor="bg-slate-300"
@@ -16,19 +27,19 @@ const Notification = () => {
         />
         <div className="w-[220px] ml-2">
           <p className="text-xs font-medium text-wrap">
-            You have received Rs 500 from ABC. dfdfdf
+            You have received Rs. {amount} from {name} on {date}.
           </p>
         </div>
       </div>
       <div className="text-center flex flex-col justify-between gap-[4px]">
         <button
-          className="p-[4px] text-sm bg-slate-300 hover:bg-slate-400  rounded-full"
+          className="p-[4px] text-sm bg-slate-300 hover:bg-slate-400 active:bg-slate-300 rounded-full"
           title="Delete"
         >
           <MdOutlineDeleteOutline />
         </button>
         <button
-          className="p-[4px] text-sm bg-slate-300 hover:bg-slate-400 rounded-full"
+          className="p-[4px] text-sm bg-slate-300 hover:bg-slate-400 active:bg-slate-300 rounded-full"
           title="Mark as read"
         >
           <IoCheckmarkDoneSharp />
--- frontend/src/components/NotificationModal.tsx
+++ frontend/src/components/NotificationModal.tsx
@@ -1,20 +1,34 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../api/axios";
 import { FaXmark } from "react-icons/fa6";
 import Notification from "./Notification";
 
+interface SenderDetails {
+  firstname: string;
+}
+
+interface Notification {
+  _id: string;
+  amount: number;
+  read: boolean;
+  createdAt: string;
+
+  senderDetails: SenderDetails;
+}
+
+interface ApiResponse {
+  notifications: Notification[];
+}
+
 const NotificationModal = () => {
-  const [notification, setNotifications] = useState(null);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
 
   const getNotifications = async () => {
     try {
-      const response = await axios.get(
-        "http://localhost:7001/api/v1/notifications",
-        {
-          withCredentials: true,
-        },
-      );
-      console.log(response.data);
+      const response = await axios.get<ApiResponse>("/notifications", {
+        withCredentials: true,
+      });
+      setNotifications(response.data?.notifications);
     } catch (error) {
       console.error(error);
     }
@@ -24,10 +38,19 @@ const NotificationModal = () => {
     getNotifications();
   }, []);
 
+  const handleDate = (timestamp: string) => {
+    const date = new Date(timestamp);
+    const day = date.getUTCDate().toString().padStart(2, "0");
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    const year = date.getUTCFullYear();
+    const formattedDate = `${day}-${month}-${year}`;
+    return formattedDate;
+  };
+
   return (
-    <div className="absolute top-10 left-1/2 right-1/2 -translate-x-1/2 bg-gray-100 w-[300px] h-[300px] rounded-md p-2 ">
+    <div className="absolute top-10 left-1/2 right-1/2 -translate-x-1/2 bg-gray-100 w-[300px] h-[300px] rounded-md p-2 shadow-black ">
       <div className="flex justify-between items-center">
-        <span className="text-black font-medium">Notifications</span>
+        <span className="text-black font-medium text-xl">Notifications</span>
         <button
           type="button"
           className="p-[4px] bg-slate-300 hover:bg-slate-400 transition duration-150 ease-in-out rounded-full"
@@ -36,11 +59,16 @@ const NotificationModal = () => {
         </button>
       </div>
       <div className="mt-2 h-[260px] notification_modal">
-        <Notification />
-        <Notification />
-        <Notification />
-        <Notification />
-        <Notification />
+        {notifications.length > 0 &&
+          notifications.map((notification) => (
+            <Notification
+              key={notification?._id}
+              amount={notification?.amount}
+              name={notification?.senderDetails?.firstname}
+              read={notification?.read}
+              date={handleDate(notification?.createdAt)}
+            />
+          ))}
       </div>
     </div>
   );
