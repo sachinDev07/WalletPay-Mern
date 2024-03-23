b9b0cbe6c56bbc72ba0ade755375c08ac6ab7afa
sachinDev07 <sachinkrj077@gmail.com>
2024-03-23 23:26:36 +0530

    Implemented update user info api functionality

--- frontend/src/pages/UpdateProfile.tsx
+++ frontend/src/pages/UpdateProfile.tsx
@@ -1,17 +1,47 @@
-import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "../api/axios";
 
 const UpdateProfile = () => {
+  const firstname = useRef<HTMLInputElement | null>(null);
+  const lastname = useRef<HTMLInputElement | null>(null);
+  const oldPassword = useRef<HTMLInputElement | null>(null);
+  const newPassword = useRef<HTMLInputElement | null>(null);
+
+  const [error, setError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.put("http://localhost:7001/api/v1/users/update", {
+        firstname: firstname.current?.value,
+        lastname: lastname.current?.value,
+        oldPassword: oldPassword.current?.value,
+        newPassword: newPassword.current?.value,
+      });
+      navigate("/login");
+    } catch (error: any) {
+      setError(error?.response?.data?.message);
+      console.error(error);
+      return;
+    }
+  };
+
   return (
     <section className="mt-4 p-4 flex flex-col justify-center items-center">
       <div className="w-full md:max-w-[500px]">
         <p className="text-center text-lg font-bold underline">
           Update Your Profile
         </p>
-        <form action="" className="mt-8 px-2 w-full">
+        <form onSubmit={handleOnSubmit} className="mt-8 px-2 w-full">
           <div className="mb-2">
             <label className="font-medium text-left py-2">
               First Name
               <input
+                ref={firstname}
+                required
                 type="text"
                 className="block w-full  px-2 py-1 border border-slate-200 rounded"
               />
@@ -21,6 +51,8 @@ const UpdateProfile = () => {
             <label className="font-medium text-left py-2">
               Last Name
               <input
+                ref={lastname}
+                required
                 type="text"
                 className="block w-full  px-2 py-1 border border-slate-200 rounded"
               />
@@ -30,6 +62,8 @@ const UpdateProfile = () => {
             <label className="font-medium text-left py-2">
               Last Password
               <input
+                ref={oldPassword}
+                required
                 type="text"
                 className="block w-full  px-2 py-1 border border-slate-200 rounded"
               />
@@ -39,12 +73,15 @@ const UpdateProfile = () => {
             <label className="font-medium text-left py-2">
               New Password
               <input
+                ref={newPassword}
+                required
                 type="text"
                 className="block w-full  px-2 py-1 border border-slate-200 rounded"
               />
             </label>
           </div>
-          <div className="mt-12 text-center">
+          <div>{error && <p className="pt-2 text-red-400">{error}</p>}</div>
+          <div className="mt-8 text-center">
             <button
               type="submit"
               className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
