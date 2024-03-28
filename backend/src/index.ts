10bdcd14274e20b466516422c6168ba0d75c78d1
sachinDev07 <sachinkrj077@gmail.com>
2024-03-28 16:01:27 +0530

    Changes for deployment on vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import ServerConfig from "./src/config/server-config";
-import connectDB from "./src/db/db";
-import userRoutes from "./src/routes/user-routes";
-import infoRoutes from "./src/routes/info-routes";
-import accountRoutes from "./src/routes/account-routes";
-import refreshTokenRoutes from "./src/routes/refreshToken-routes";
-import notificationRoutes from "./src/routes/notifications-routes";
+import ServerConfig from "./config/server-config";
+import connectDB from "./db/db";
+import userRoutes from "./routes/user-routes";
+import infoRoutes from "./routes/info-routes";
+import accountRoutes from "./routes/account-routes";
+import refreshTokenRoutes from "./routes/refreshToken-routes";
+import notificationRoutes from "./routes/notifications-routes";
 
 const app = express();
 
