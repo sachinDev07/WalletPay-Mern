99cac14c38c6cd298bc11071c1d352e1cabf44d7
sachinDev07 <sachinkrj077@gmail.com>
2024-03-31 14:05:11 +0530

    Changed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,12 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
