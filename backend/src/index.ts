ab247625de3d764ad6db0ef76aca895c52b3a1c9
sachinDev07 <sachinkrj077@gmail.com>
2024-03-31 18:54:16 +0530

    Added frontend link

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://wallet-pay.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
