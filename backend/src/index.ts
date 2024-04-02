d0455e372b91af3c09dd22291e8a09416fb5d916
sachinDev07 <sachinkrj077@gmail.com>
2024-04-02 14:14:18 +0530

    Changed url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://wallet-pay.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
