e11a8701080c854e161f6e8767be3ca21a4ad458
sachinDev07 <sachinkrj077@gmail.com>
2024-04-03 17:22:34 +0530

    Changed style of toast for small device

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -8,7 +8,7 @@ type ToastProps = {
 const Toast = ({ message, type, onClose }: ToastProps) => {
   useEffect(() => {
     const timer = setTimeout(() => {
-      onClose();
+        onClose();
     }, 3000);
 
     return () => clearTimeout(timer);
@@ -16,13 +16,13 @@ const Toast = ({ message, type, onClose }: ToastProps) => {
 
   const styles =
     type === "SUCCESS"
-      ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
-      : "fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md";
+      ? "fixed top-4 right-4 z-50 p-2 md:p-4 rounded-md bg-green-600 text-white max-w-md"
+      : "fixed top-4 right-4 z-50 p-2 md:p-4 rounded-md bg-red-600 text-white max-w-md";
 
   return (
     <div className={styles}>
       <div className="flex justify-center items-center">
-        <span className="text-lg font-semibold">{message}</span>
+        <span className="text-xs md:text-lg font-semibold">{message}</span>
       </div>
     </div>
   );
