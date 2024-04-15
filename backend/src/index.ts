0bdc41e85bebeea0a0b101e708370e1b82a57937
sachinDev07 <sachinkrj077@gmail.com>
2024-04-15 22:56:56 +0530

    changed cors url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ServerConfig.FRONTEND_URL,
     credentials: true,
   }),
 );
