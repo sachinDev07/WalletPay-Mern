9a5d7baf207c18e50911059c25292d5a1c27719c
sachinDev07 <sachinkrj077@gmail.com>
2024-04-15 22:56:11 +0530

    forecelly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import SendMoney from "./pages/SendMoney";
-import PersistLogin from "./components/PersistentLogin";
-import RequiredAuth from "./components/RequiredAuth";
 import Layout from "./components/Layout";
 import UserProfileModalProvider from "./context/UserProfileContext";
 import UpdateProfile from "./pages/UpdateProfile";
@@ -13,23 +11,22 @@ import UpdateProfile from "./pages/UpdateProfile";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<UserProfileModalProvider><Layout /></UserProfileModalProvider>}>
+      <Route
+        path="/"
+        element={
+          <UserProfileModalProvider>
+            <Layout />
+          </UserProfileModalProvider>
+        }
+      >
         <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
 
-        <Route element={<PersistLogin />}>
-          <Route element={<RequiredAuth allowedRole="user" />}>
-            <Route path="/" element={<Dashboard />} />
-          </Route>
+        <Route path="/" element={<Dashboard />} />
 
-          <Route element={<RequiredAuth allowedRole="user" />}>
-            <Route path="/send" element={<SendMoney />} />
-          </Route>
+        <Route path="/send" element={<SendMoney />} />
 
-          <Route element={<RequiredAuth allowedRole="user" />}>
-            <Route path="/update-profile" element={<UpdateProfile />} />
-          </Route>
-        </Route>
+        <Route path="/update-profile" element={<UpdateProfile />} />
       </Route>
     </Routes>
   );
