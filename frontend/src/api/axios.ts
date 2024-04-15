0de15dd451722ed6c102c206e373e77c92f6722e
sachinDev07 <sachinkrj077@gmail.com>
2024-04-16 00:09:55 +0530

    Added backend api

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://walletpay-lgaj.onrender.com/api/v1";
+export const BASE_URL = "https://walletpay-qq4j.onrender.com/api/v1";
 
 
 export default axios.create({
