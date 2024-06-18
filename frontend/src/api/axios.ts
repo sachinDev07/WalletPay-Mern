6df233172635e5ebcfa8ec83edb4f0fbac84b3e5
sachinDev07 <sachinkrj077@gmail.com>
2024-06-18 13:20:10 +0530

    added url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const BASE_URL = "https://walletpay-mern-5oft.onrender.com/api/v1";
+
 
 export default axios.create({
   baseURL: BASE_URL,
